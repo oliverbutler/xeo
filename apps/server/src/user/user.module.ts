c29d098b43eb3619fbd0da5f1b563314f11e107f
Oliver Butler <dev@oliverbutler.uk>
2021-11-22 15:55:48 +0000

    ♻️ refactor user module to remove adapter

--- apps/server/src/user/user.module.ts
+++ apps/server/src/user/user.module.ts
@@ -2,12 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { BlockModule } from '../block/block.module';
 import { UserResolver } from './user.resolver';
-import { UserAdapter } from './user.adapter';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [forwardRef(() => BlockModule), PrismaModule],
-  providers: [UserService, UserResolver, UserAdapter],
+  providers: [UserService, UserResolver],
   exports: [UserService],
 })
 export class UserModule {}
--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,24 +1,50 @@
-import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
-import { CreateUserDto, UserAdapter } from './user.adapter';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userAdapter: UserAdapter) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   public async getAll(): Promise<User[]> {
-    return await this.userAdapter.getAll();
+    return await this.prismaService.user.findMany();
   }
 
   public async getById(id: User['id']): Promise<User> {
-    return await this.userAdapter.getById(id);
+    const user = await this.prismaService.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new BadRequestException(`UserAdapter > User  ${id} not found`);
+    }
+
+    return user;
   }
 
   public async getByUsername(username: User['username']): Promise<User> {
-    return await this.userAdapter.getByUsername(username);
+    const user = await this.prismaService.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new BadRequestException(
+        `UserAdapter > User  ${username} not found`
+      );
+    }
+
+    return user;
   }
 
-  public async create(user: CreateUserDto): Promise<User> {
-    return await this.userAdapter.createUser(user);
+  public async create(user: Prisma.UserUncheckedCreateInput): Promise<User> {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: { username: user.username },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException(
+        `UserAdapter > User ${user.username} already exists`
+      );
+    }
+
+    return await this.prismaService.user.create({ data: user });
   }
 }
