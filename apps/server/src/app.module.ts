53f307aa877226487174488c418cab8fa1c93ec0
Oliver Butler <dev@oliverbutler.uk>
2021-11-22 14:09:25 +0000

    ✨ Add prisma and move app module

---
+++ apps/server/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { BlockModule } from './block/block.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'apps/server/src/graphql.ts'),
+      },
+      resolvers: {},
+    }),
+    PrismaModule,
+    UserModule,
+    BlockModule,
+    AuthModule,
+  ],
+  providers: [],
+  exports: [],
+})
+export class AppModule {}
