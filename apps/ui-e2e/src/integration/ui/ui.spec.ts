b2c504f10b691a5101743404a29cfe77d69096bf
Oliver Butler <dev@oliverbutler.uk>
2021-12-17 01:06:30 +0000

    ⚰️ remove ui-e2e

--- apps/ui-e2e/src/integration/ui/ui.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('ui: Ui component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=ui--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to Ui!');
-    });
-});
