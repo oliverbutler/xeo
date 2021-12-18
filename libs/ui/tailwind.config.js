b77e8e3a8fb4afed6208e5c2f7f038d20a0be634
Oliver Butler <dev@oliverbutler.uk>
2021-12-18 14:27:29 +0000

    🐛 fix Tailwind issue (remove NX purge)

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,14 +1,9 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  // presets: [require('../../../tailwind-workspace-preset.js')],
-  purge: [
-    join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
   content: [join(__dirname, './src/**/*.{js,ts,jsx,tsx}')],
-  darkMode: 'media',
+  darkMode: 'class',
+  mode: 'jit',
   theme: {
     extend: {},
   },
