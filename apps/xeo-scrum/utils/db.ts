8f75b3f5b3bbdfc54b8b785b697d9844d645fcda
Oliver Butler <dev@oliverbutler.uk>
2022-02-09 23:14:19 +0000

    🚀 Make PrismaClient global

--- apps/xeo-scrum/utils/db.ts
+++ apps/xeo-scrum/utils/db.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
