b9a39b289235351fcd95a56deebd46a3e3b4d216
Oliver Butler <dev@oliverbutler.uk>
2022-02-09 23:45:56 +0000

    🚀Add post build step to geneate prisma client for xeo-scrum

--- apps/xeo-scrum/utils/db.ts
+++ apps/xeo-scrum/utils/db.ts
@@ -5,8 +5,6 @@ declare global {
   var prisma: PrismaClient;
 }
 
-const prisma = global.prisma || new PrismaClient();
+export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
-
-export default prisma;
