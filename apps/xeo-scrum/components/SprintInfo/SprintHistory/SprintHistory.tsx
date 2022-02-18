7de1e7f8ba884ad07113dc24400d00b86396950e
Oliver Butler <dev@oliverbutler.uk>
2022-02-18 00:06:27 +0000

    ✨ Add history table

--- apps/xeo-scrum/components/SprintInfo/SprintHistory/SprintHistory.tsx
+++ apps/xeo-scrum/components/SprintInfo/SprintHistory/SprintHistory.tsx
@@ -1,13 +1,52 @@
 import { Sprint } from '@prisma/client';
+import { Error } from 'components/Error/Error';
+import {
+  GetSprintHistory,
+  SprintHistoryWithStatus,
+} from 'pages/api/sprint/[sprintId]/history';
+import { useQuery } from 'utils/api';
+import { CentredLoader, Table } from '@xeo/ui';
+import dayjs from 'dayjs';
+import { groupBy } from '@xeo/utils';
 
 interface Props {
   sprint: Sprint;
 }
 
 export const SprintHistory: React.FunctionComponent<Props> = ({ sprint }) => {
+  const { data, error, isLoading } = useQuery<GetSprintHistory>(
+    `/api/sprint/${sprint.id}/history`
+  );
+
+  if (isLoading) {
+    return <CentredLoader />;
+  }
+
+  if (error || !data) {
+    return (
+      <Error errorMessage="Error fetching sprint history, please try again!" />
+    );
+  }
+
+  const historyByDay = groupBy(data.sprintHistory, (item) =>
+    dayjs(item.timestamp).format('YYYY-MM-DD')
+  );
+
+  console.log(historyByDay);
+
   return (
     <div>
       <h2>History</h2>
+      <Table<SprintHistoryWithStatus>
+        columns={[
+          {
+            Header: 'Time',
+            accessor: 'timestamp',
+            Cell: (row) => dayjs(row.value).format('DD/MM/YY HH:mm'),
+          },
+        ]}
+        data={data.sprintHistory}
+      />
     </div>
   );
 };
---
+++ apps/xeo-scrum/pages/api/sprint/[sprintId]/history.ts
@@ -0,0 +1,59 @@
+import { SprintHistory, SprintStatusHistory } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSentry } from '@sentry/nextjs';
+import { apiError, APIGetRequest, apiResponse } from 'utils/api';
+import { getSession } from 'next-auth/react';
+
+export type GetSprintHistory = APIGetRequest<{
+  sprintHistory: SprintHistoryWithStatus[];
+}>;
+
+export type SprintHistoryWithStatus = SprintHistory & {
+  sprintStatusHistory: SprintStatusHistory[];
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
+
+  const sprintId = req.query.sprintId as string;
+  const userId = session.id;
+
+  if (req.method === 'GET') {
+    const sprintHistory = await prisma.sprintHistory.findMany({
+      where: {
+        sprint: {
+          id: sprintId,
+          backlog: {
+            members: {
+              some: {
+                user: {
+                  id: userId,
+                },
+              },
+            },
+          },
+        },
+      },
+      include: {
+        sprintStatusHistory: true,
+      },
+      orderBy: {
+        timestamp: 'desc',
+      },
+    });
+
+    if (!sprintHistory) {
+      return apiError(res, { message: 'Sprint not found' }, 404);
+    }
+
+    return apiResponse(res, { sprintHistory });
+  }
+
+  return apiError(res, { message: 'Not implemented' }, 501);
+};
+
+export default withSentry(handler);
