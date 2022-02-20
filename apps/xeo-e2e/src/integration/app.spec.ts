ba430816f4795a911b79653a798d01a6309cebc8
Oliver Butler <dev@oliverbutler.uk>
2022-02-20 18:52:15 +0000

    ♻️ Refactor xeo-scrum to xeo

--- apps/xeo-e2e/src/integration/app.spec.ts
+++ apps/xeo-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('xeo-scrum', () => {
+describe('xeo', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('xeo-scrum', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome xeo-scrum');
+    getGreeting().contains('Welcome xeo');
   });
 });
--- apps/xeo/jest.config.js
+++ apps/xeo/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'xeo-scrum',
+  displayName: 'xeo',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/xeo-scrum',
+  coverageDirectory: '../../coverage/apps/xeo',
 };
