50fb62b0df4cb04bda681d7437e42f5de1750452
Oliver Butler <dev@oliverbutler.uk>
2022-02-21 22:27:29 +0000

    💚 Fix Prisma client type

--- apps/xeo/utils/db.ts
+++ apps/xeo/utils/db.ts
@@ -2,6 +2,7 @@
 
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = (global as any).prisma || new PrismaClient();
+export const prisma: PrismaClient =
+  (global as any).prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') (global as any).prisma = prisma;
