7bf8d08f5db24f85e8a7b0f47ce6e471bfc6309d
Oliver Butler <dev@oliverbutler.uk>
2022-02-23 00:18:24 +0000

    💄 Fix Showcase colours in dark mode

--- apps/xeo/components/Showcase/Showcase.tsx
+++ apps/xeo/components/Showcase/Showcase.tsx
@@ -74,16 +74,15 @@ export const Showcase: React.FunctionComponent = () => {
             ].map((feature) => (
               <div key={feature.name} className="relative">
                 <dt>
-                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-secondary-500 text-white">
-                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-secondary-500 ">
+                    <feature.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
                   </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    {feature.name}
-                  </p>
+                  <h3 className="ml-16">{feature.name}</h3>
                 </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  {feature.description}
-                </dd>
+                <p className="mt-2 ml-16 ">{feature.description}</p>
               </div>
             ))}
           </dl>
