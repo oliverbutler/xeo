0f7443f89934ea79871aed663e272c8a1c1767aa
Oliver Butler <dev@oliverbutler.uk>
2022-03-06 19:23:49 +0000

    🚀 Add AWS cdk

--- apps/xeo/pages/api/auth/[...nextauth].ts
+++ apps/xeo/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
-import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { prisma } from 'utils/db';
+import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+import NextAuth from 'next-auth';
+import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter';
 
 if (!process.env.GITHUB_ID || !process.env.GITHUB_SECRET) {
   throw new Error('GITHUB_ID and GITHUB_SECRET must be set in .env');
@@ -18,9 +19,25 @@ declare module 'next-auth' {
   }
 }
 
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY as string,
+  },
+  region: process.env.NEXT_AUTH_AWS_REGION,
+};
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+});
+
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
+  adapter: DynamoDBAdapter(client, { tableName: 'xeo' }),
   callbacks: {
     session: async ({ session, user }) => {
       session.id = user.id;
