c714a7d390d2f86c808ec8071b28121d0d17654d
Oliver Butler <dev@oliverbutler.uk>
2022-03-06 19:23:49 +0000

    🚀 Add AWS cdk

---
+++ apps/cdk/lib/cdk-stack.ts
@@ -0,0 +1,20 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new dynamodb.Table(this, `XeoTable`, {
+      tableName: 'xeo',
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+      timeToLiveAttribute: 'expires',
+    }).addGlobalSecondaryIndex({
+      indexName: 'GSI1',
+      partitionKey: { name: 'GSI1PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'GSI1SK', type: dynamodb.AttributeType.STRING },
+    });
+  }
+}
--- apps/xeo/next.config.js
+++ apps/xeo/next.config.js
@@ -7,6 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
   },
