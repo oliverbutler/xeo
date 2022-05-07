69dd74729f5dee56a3904e53a85baa6e8d61c9b2
Oliver Butler <dev@oliverbutler.uk>
2022-05-07 18:15:17 +0100

    🐛 Fix TeamAvatars height

--- apps/xeo/components/Team/Teams/TeamAvatars.tsx
+++ apps/xeo/components/Team/Teams/TeamAvatars.tsx
@@ -6,11 +6,11 @@ interface Props {
 
 export const TeamAvatars: React.FunctionComponent<Props> = ({ team }) => {
   return (
-    <div className="-space-x-4">
+    <div className="h-10">
       {team.members.map((member) => (
         <img
           key={member.userId}
-          className="relative z-30 inline object-cover w-10 h-10 rounded-full"
+          className="relative z-30 my-0 inline object-cover w-6 h-6 rounded-full"
           src={member?.user?.image || ''}
           alt="Profile image"
         />
